74e8d45e867fe023b9f4a903ec8652116b389139
srisharan kenam <36810338+sharan59@users.noreply.github.com>
2018-07-17 13:52:23 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,12 +18,11 @@ update:boolean=false;
 datatobeedited:any;
 
 table : any;
- dps : any;
+ 
 
 constructor(){
   this.table=[{sno:"1",month:"jan",expense:"10000",leftover:"2500"}]
-  this.dps=[{expense:"10000"},
-{expense:"5000"}]
+  
 }
 
 public barChartLabels:any[] = ["January","February","March","April","May","June","July"];
@@ -58,12 +57,9 @@ let up : any;
 newo = {
   sno: this.sno, month : this.month, expense : this.expense, leftover: this.leftover
 }
-// up = {
-//   expense : this.expense
-// }
-
+  
  this.table.push(newo);
-// this.dps.push(up);
+
 console.log(this.table)
 
 this.setchartdata(this.table)
@@ -71,8 +67,7 @@ this.setchartdata(this.table)
 
 setchartdata(table){
   let data = [];
- 
- 
+  
   table.map((row)=>{data[row.month]=(data[row.month]||0)+row.expense})
   this.barChartLabels= Object.keys(data);
   this.barChartData = Object.values(data);
@@ -87,9 +82,8 @@ deleteData(x:number)
     this.table.splice(x,1)
     this.setchartdata(this.table)
   }
-  editData(i:any)
+ editData(i:any)
   {
-   
     if(this.update==true)
     {
       this.update=false;
